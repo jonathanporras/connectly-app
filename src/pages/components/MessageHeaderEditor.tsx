1b414efa23869a99163d9caeba6c71673fee0135
jonathanporras <porrasjon@gmail.com>
2024-06-26 14:14:20 -0700

    chore: add dropdown to messgae builder

--- src/pages/components/MessageHeaderEditor.tsx
+++ src/pages/components/MessageHeaderEditor.tsx
@@ -5,6 +5,12 @@ import Typography from "@mui/material/Typography";
 import ImageIcon from "@mui/icons-material/Image";
 import ErrorIcon from "@mui/icons-material/Error";
 import Switch from "@mui/material/Switch";
+import FormHelperText from "@mui/material/FormHelperText";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Box from "@mui/material/Box";
+import { Button } from "@mui/material";
 
 const label = { inputProps: { "aria-label": "Message Header Switch" } };
 
@@ -29,6 +35,21 @@ export default function MessageHeaderEditor() {
         {...label}
         defaultChecked
       />
+      <FormControl sx={{ m: 1, minWidth: 80 }}>
+        <Select
+          labelId="demo-simple-select-autowidth-label"
+          id="demo-simple-select-autowidth"
+          value={"Image"}
+          onChange={() => null}
+          autoWidth
+        >
+          <MenuItem value="Image">Image</MenuItem>
+        </Select>
+        <FormHelperText>
+          Image size recommendation: 800 x 418 pixel.
+        </FormHelperText>
+      </FormControl>
+      <Button variant="outlined">Upload Image</Button>
     </div>
   );
 }
